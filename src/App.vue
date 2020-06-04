47e17262d5a315b4264b874120e05de69b316225
Howard <howard89.chung@gmail.com>
2020-06-04 19:40:12 +0800

    cd

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand mr-5" href="#">舊愛贈</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active mr-5">
+              <router-link class="nav-link" to="/">首頁</router-link>
+            </li>
+            <li class="nav-item mr-5">
+              <router-link class="nav-link" to="/about">我要捐贈</router-link>
+            </li>
+            <li class="nav-item mr-5">
+              <router-link class="nav-link" to="/about">公益單位合作</router-link>
+            </li>
+            <li class="nav-item mr-5">
+              <router-link class="nav-link" to="/about">我要捐款</router-link>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <li class="nav-item mr-3" >
+                <a class="btn btn-outline-primary" href="login.html" role="button">登入</a>
+              </li>
+              <li class="nav-item">
+                <a class="btn btn-outline-primary" href="signin.html" role="button">註冊</a>
+              </li>
+          </form>
+        </div>
+      </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+      <div class="banner mb-3 clearfix">
+        <img src="../assets/banner1.png" class="img-fluid">
+        <!-- <div class="ban-text">舊物傳情</div>
+        <div class="ban-text2">本平台期望透過便利的金物流及捐贈單管理</div>
+        <div class="ban-text3">使閒置資源再次流動，正確地將物資分享給需要的人</div> -->
+      </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+}
+</script>
