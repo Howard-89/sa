b2067e0a71e85691b3b2d8774ec88fb86d1f1c0a
duwn <nick31914@gmail.com>
2020-06-06 21:54:54 +0800

    Gino78

---
+++ src/views/Confirm.vue
@@ -0,0 +1,78 @@
+<template>
+    <div id="donation">
+        <div class="main py-5" style="background-color: rgb(246, 233, 205);">
+            <div class="border border-dark shadow  mb-5 bg-white rounded mx-auto" style=" width: 65%;">
+                
+                <div class="form-row mx-0 mb-3"
+                    style="background-color:rgb(48, 172, 85); width: 100%; height: 50px ;color:rgb(255, 255, 255);">
+                    <div class="mt-3 mx-auto">確認捐贈</div>
+                </div>
+                <div class="form-group px-3">
+                    <label for="inputInformation" class="mb-2">個人資訊</label>
+                    <div class="name mt-3" style="font-size:30px">Collin</div> 
+                </div>
+                <div class="form-group px-3">
+                    <label for="inputThing" class="mb-2">物資名稱</label>
+                    <input type="text" class="form-control mx-auto" style="width:700px">
+                </div>
+
+                <div class="form-group px-3">
+                    <label for="inputAmount" class="mb-2">捐贈數量</label>
+                    <input type="text" class="form-control mx-auto" style="width:700px">
+                </div>
+
+                <div class="form-group px-3">
+                    <label for="inputAddress1" class="mb-2">單位地址</label>
+                    <input type="text" class="form-control mx-auto" style="width:700px" >
+                </div>
+
+                <div class="form-group px-3">
+                    <label for="inputName" class="mb-2">單位名稱</label>
+                    <input type="text" class="form-control mx-auto" style="width:700px" >
+                </div>
+
+                <div class="form-group px-3">
+                    <label for="inputTel" class="mb-2">電話</label>
+                    <input type="text" class="form-control mx-auto" style="width:700px" >
+                </div>
+                
+                <div class="form-group px-3">
+                    <label for="inputInformation1" class="mb-2">資訊</label>
+                    <input type="text" class="form-control mx-auto" style="width:700px" >
+                </div>
+
+                <div class="form-group px-3">
+                        <label for="inputPassword4" class="mb-2">捐贈方式</label>
+                        <select class="custom-select d-block mx-auto" style="width:700px" >
+                            <option selected>選擇...</option>
+                            <option value="1">四大超商郵寄</option>
+                            <option value="2">自行送達</option>
+                            <option value="3">宅配</option>
+                            <option value="4">聯絡公益單位取貨</option>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-danger mx-auto mb-3" style="display: block">確認送出</button>
+
+              
+                
+               
+
+
+
+            
+            
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/views/History.vue
@@ -0,0 +1,56 @@
+<template>
+  <div style="height:500px" >
+        <div class="main py-5" style="background-color: rgb(246, 233, 205);">
+            <div class="border border-dark shadow  mb-5 bg-white rounded mx-auto" style=" width: 65%;">
+                <div class="form-row mx-0 mb-4"
+                    style="background-color:rgb(48, 172, 85); width: 100%; height: 50px ;color:rgb(255, 255, 255);">
+                    <div class="mt-3 mx-auto">歷史紀錄</div>
+                </div>
+  
+            <img src="../assets/history1.png" class="card-img-top" style="width:150px; height: 150px ;">
+            <hr>
+            <div class="row" style="text-align:left;line-height:70px;">
+                <div class="col-3 mt-5" ></div>
+                <div class="col-3"> <div class="name"><img src="../assets/donate.png" style="width:18px; height: 18px ;" class="mr-2">捐贈紀錄</div> </div>
+                <div class="col-6 mb-5">
+                曾於2019.8.28捐贈1台電冰箱給慈祐基金會
+                <br>
+                曾於2019.8.28捐贈1台電冰箱給慈祐基金會
+                 <br>
+                曾於2019.8.28捐贈1台電冰箱給慈祐基金會
+                </div>
+              
+         
+                <div class="col-3 mt-5" ></div>
+                <div class="col-3"> <div class="name"><img src="../assets/money.png" style="width:18px; height: 18px ;" class="mr-2">捐款紀錄</div> </div>
+                <div class="col-6 mb-5">
+                曾於2020.10.25捐款一萬元整給關愛之家
+                <br>
+                曾於2020.10.25捐款一萬元整給關愛之家
+                <br>
+                曾於2020.10.25捐款一萬元整給關愛之家
+                </div>
+                
+             
+            </div>
+               
+                
+              
+           
+
+             
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
